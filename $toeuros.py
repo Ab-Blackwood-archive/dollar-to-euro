1e855052eb786f092d23ca0af56b34da3f4e5b53
Ab-Blackwood-archive <ab061906@wgcloud.org>
2023-10-27 11:03:36 -0500

    finish loop+calc

--- $toeuros.py
+++ $toeuros.py
@@ -1,3 +1,18 @@
+"""""
+this program takes the dollar amount entered by the user and converts it to euros
+made by acadia babcook
+"""
+Dollar = 0
+Euros = 0
+def Converter():
+    Dollar= float(input("Enter dollar amount to be converted: "))
+    Euros = Dollar* .94540
+    print("$" + str(Dollar)+" Dollars" + " is equivelant to:  %s"%(u"\N{euro sign}") + str(Euros) + " Euros")
 # title
-print" Dollar
-#
+print ("Dollar to Euro money converter")
+while True:
+    userinput = input("Would you like to convert dollars to euros? Yes or No ")
+    if userinput == "yes":
+        Converter()
+    else:
+        break
